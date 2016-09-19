9fac77c4fd6480813f67748b734738bb548211df
lukew244 <luke.webster86@gmail.com>
2016-09-19 11:16:16 +0100

    adds balance

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,9 +1,21 @@
 class Bank
 
-  attr_reader :menu
+  attr_reader :menu, :balance
 
   def initialize
-    @menu = []
+    @menu = ["Withdraw", "Deposit", "Balance", "Exit"]
+    @balance = 0
+    # print_menu
+  end
+
+  def print_menu
+    @menu.each do |i|
+      puts i
+    end
+
+
   end
 
 end
+
+# bank = Bank.new
--- spec/bank_app_spec.rb
+++ spec/bank_app_spec.rb
@@ -5,7 +5,17 @@ describe Bank do
 
   describe 'menu' do
     it 'contains a list of items' do
-      expect(bank.menu).to eq([])
+      expect(bank.menu).to include("Exit")
+    end
+
+    it 'prints out the list on initialize' do
+      expect { bank.print_menu }.to output(/Exit/).to_stdout
+    end
+  end
+
+  describe 'balance' do
+    it 'has a starting balance of zero' do
+      expect(bank.balance).to eq 0
     end
 
   end
